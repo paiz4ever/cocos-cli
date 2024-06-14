41f5a83b955c86e16540e200b48e18321391da13
paiz4ever <1151892501@qq.com>
2024-06-14 16:16:55 +0800

    feat: 去除模版

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,6 @@
 export const Templates = [
   {
-    name: "2D空模板",
-    url: "paiz4ever/cocos-framework#2d",
-  },
-  {
-    name: "3D空模板",
-    url: "paiz4ever/cocos-framework#3d",
+    name: "空白模板",
+    url: "paiz4ever/cocos-framework",
   },
 ];
